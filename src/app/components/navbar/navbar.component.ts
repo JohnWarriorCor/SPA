225a1b94203d8f619f0e08801eef4b7cd534a220
JohnWarriorCor <guerrerocordobajohn@gmail.com>
2019-07-28 18:44:02 -0500

    Requisitos funcionales basicos terminados 28/07/2019

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { AuthService } from "../../services/auth.service";
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styles: []
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  profile: any;
 
   constructor( private auth: AuthService ) {
     auth.handleAuthentication();
@@ -21,8 +22,17 @@ export class NavbarComponent {
   salir() {
     this.auth.logout();
   }
+  ngOnInit() {
 
+    if (this.auth.userProfile) {
+      this.profile = this.auth.userProfile;
+      console.log(this.profile);
+    } else {
+      this.auth.getProfile((err, profile) => {
+        this.profile = profile;
+        console.log(this.profile);
+      });
+    }
 
-
-
+  }
 }
